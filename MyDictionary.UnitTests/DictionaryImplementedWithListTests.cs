2c520a28296d4cd35f4d03aa5ba4a7d28a156626
unknown <wwang@wwang-e7440.FranklinSquare.local>
2016-03-18 10:42:15 -0400

    first commit

---
+++ MyDictionary.UnitTests/DictionaryImplementedWithListTests.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using FluentAssertions;
+
+namespace MyDictionary.UnitTests
+{
+    public class Animal
+    {
+        public string Name;
+        public string Says;
+    }
+
+    // only compares on what the Animal says, NOT the name
+    public class AnimalComparer : IEqualityComparer<Animal>
+    {
+        public bool Equals(Animal x, Animal y)
+        {
+            return x.Says.Equals(y.Says);
+        }
+
+        public int GetHashCode(Animal obj)
+        {
+            return obj.GetHashCode(); 
+        }
+    }
+
+    [TestClass]
+    public class DictionaryImplementedWithListTests
+    {
+        [TestMethod]
+        public void Add_KeyNotExist_IncreasesCount()
+        {
+            var dctIntStr = new DictionaryImplementedWithList<int, string>();
+
+            dctIntStr.Count.Should().Be(0);
+            dctIntStr.Add(new KeyValuePair<int, string>(1, "A"));
+            dctIntStr.Count.Should().Be(1);
+        }
+
+        [TestMethod]
+        public void Add_KeyExists_ThrowsError()
+        {
+            var dctIntStr = new DictionaryImplementedWithList<int, string>();
+            
+            dctIntStr.Add(new KeyValuePair<int, string>(1, "A"));
+            Action act = () => dctIntStr.Add(new KeyValuePair<int, string>(1, "A"));
+            act.ShouldThrow<ArgumentException>();
+        }
+
+        [TestMethod]
+        public void Clear_ReducesCountToZero()
+        {
+            var dctIntStr = new DictionaryImplementedWithList<int, string>();
+
+            dctIntStr.Add(1, "A");
+            dctIntStr.Count.Should().Be(1);
+
+            dctIntStr.Clear();
+            dctIntStr.Count.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void DictionaryGet_KeyNotExists_ThrowsKeyNotFound()
+        {
+            var dctIntStr = new DictionaryImplementedWithList<int, string>();
+            string val;
+            Action act = () => val = dctIntStr[3];
+
+            act.ShouldThrow<KeyNotFoundException>();
+        }
+
+        [TestMethod]
+        public void DictionaryGet_KeyExists_ReturnsValue()
+        {
+            var dctIntStr = new DictionaryImplementedWithList<int, string>();
+
+            dctIntStr.Add(1, "A");
+            var val = dctIntStr[1];
+
+            val.Should().Be("A");
+        }
+
+        [TestMethod]
+        public void DictionarySet_KeyNotExists_ThrowsKeyNotFound()
+        {
+            var dctIntStr = new DictionaryImplementedWithList<int, string>();
+
+            dctIntStr.Add(1, "a");
+
+            Action act = () => dctIntStr[2] = "A";
+            act.ShouldThrow<KeyNotFoundException>();
+        }
+
+        [TestMethod]
+        public void DictionarySet_KeyExists_SetsValue()
+        {
+            var dctIntStr = new DictionaryImplementedWithList<int, string>();
+
+            dctIntStr.Add(1, "a");
+            dctIntStr[1] = "b";
+
+            dctIntStr[1].Should().Be("b");
+        }
+
+        [TestMethod]
+        public void ContainsKey_StringKey_UsesCaseInsensitiveCompare_KeyExists_ReturnsTrue()
+        {
+            var dctStrStr = new DictionaryImplementedWithList<string, string>();
+
+            dctStrStr.Add("mykey", "myvalue");
+            dctStrStr.ContainsKey("MYKEY").Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void ContainsKey_ObjectKey_UseCustomComparer_KeyExists_ReturnsTrue()
+        {
+            var dctObjStr = new DictionaryImplementedWithList<Animal, string>(
+                new AnimalComparer());
+            var tony = new Animal { Name = "Tony", Says = "Roar" };
+            var tigger = new Animal { Name = "Tigger", Says = "Roar" };
+
+            dctObjStr.Add(tony, "they're great!");
+            dctObjStr.ContainsKey(tigger).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void ContainsKey_ObjectKey_UseCustomComparer_KeyNotExists_ReturnsFalse()
+        {
+            var dctObjStr = new DictionaryImplementedWithList<Animal, string>(
+                new AnimalComparer());
+            var tony = new Animal { Name = "Tony", Says = "Roar" };
+            var simba = new Animal { Name = "Tigger", Says = "Meow" };
+
+            dctObjStr.Add(tony, "they're great!");
+            dctObjStr.ContainsKey(simba).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void ContainsKey_NonSpecialKey_KeyExists_ReturnsTrue()
+        {
+            var dctIntStr = new DictionaryImplementedWithList<int, string>();
+
+            dctIntStr.Add(1, "a");
+            dctIntStr.ContainsKey(1).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void ContainsKey_NonSpecialKey_KeyNotExists_ReturnsFalse()
+        {
+            var dctIntStr = new DictionaryImplementedWithList<int, string>();
+
+            dctIntStr.Add(1, "a");
+            dctIntStr.ContainsKey(2).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void CopyTo_CopiesDictionaryToArray()
+        {
+            var myArray = new KeyValuePair<int, string>[5];
+            var dctIntStr = new DictionaryImplementedWithList<int, string>();
+
+            dctIntStr.Add(1, "a");
+            dctIntStr.Add(2, "b");
+            dctIntStr.Add(3, "c");
+
+            dctIntStr.CopyTo(myArray, 2);
+
+            myArray[2].Key.Should().Be(1);
+            myArray[2].Value.Should().Be("a");
+            myArray[3].Key.Should().Be(2);
+            myArray[3].Value.Should().Be("b");
+            myArray[4].Key.Should().Be(3);
+            myArray[4].Value.Should().Be("c");
+        }
+
+        [TestMethod]
+        public void GetEnumerator_CanEnumerate()
+        {
+            var dctIntStr = new DictionaryImplementedWithList<int, string>();
+
+            dctIntStr.Add(1, "a");
+            dctIntStr.Add(2, "b");
+            dctIntStr.Add(3, "c");
+
+            var enumerator = dctIntStr.GetEnumerator();
+            var counter = 1;
+            while(enumerator.MoveNext())
+            {
+                var pair = enumerator.Current;
+                pair.Key.Should().Be(counter);
+                counter++;
+            }
+        }
+
+        [TestMethod]
+        public void RemoveItem_ItemExists_ReturnsTrue()
+        {
+            var dctIntStr = new DictionaryImplementedWithList<int, string>();
+
+            dctIntStr.Add(1, "a");
+            dctIntStr.Remove(new KeyValuePair<int, string>(1, "a")).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void RemoveItem_ItemNotExists_ReturnsFalse()
+        {
+            var dctIntStr = new DictionaryImplementedWithList<int, string>();
+
+            dctIntStr.Add(1, "a");
+            dctIntStr.Remove(new KeyValuePair<int, string>(2, "a")).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void RemoveByKey_KeyExists_ReturnsTrue()
+        {
+            var dctIntStr = new DictionaryImplementedWithList<int, string>();
+
+            dctIntStr.Add(1, "a");
+            dctIntStr.Remove(1).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void RemoveByKey_KeyNotExists_ReturnsFalse()
+        {
+            var dctIntStr = new DictionaryImplementedWithList<int, string>();
+
+            dctIntStr.Add(1, "a");
+            dctIntStr.Remove(2).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void TryGetValue_KeyExists_ReturnTrue()
+        {
+            var dctIntStr = new DictionaryImplementedWithList<int, string>();
+            string myValue;
+
+            dctIntStr.Add(1, "a");
+
+            dctIntStr.TryGetValue(1, out myValue).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TryGetValue_KeyNotExists_ReturnFalse()
+        {
+            var dctIntStr = new DictionaryImplementedWithList<int, string>();
+            string myValue;
+
+            dctIntStr.Add(1, "a");
+
+            dctIntStr.TryGetValue(2, out myValue).Should().BeFalse();
+        }
+    }
+}
---
+++ MyDictionary/DictionaryImplementedWithList.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    public class DictionaryImplementedWithList<TKey, TValue> : IDictionary<TKey, TValue>
+    {
+        internal class CaseInsensitiveStringComparer : IEqualityComparer<TKey>
+        {
+            public bool Equals(TKey x, TKey y)
+            {
+                if (typeof(TKey) == typeof(string))
+                {
+                    var stringX = (string)Convert.ChangeType(x, typeof(string));
+                    var stringY = (string)Convert.ChangeType(y, typeof(string));
+
+                    return (stringX.Equals(stringY, StringComparison.OrdinalIgnoreCase));
+                }
+                return x.Equals(y);
+            }
+
+            public int GetHashCode(TKey obj)
+            {
+                return obj.GetHashCode();
+            }
+        }
+        private readonly List<TKey> internalKeys;
+        private readonly List<TValue> internalValues;
+        private int count;
+        private readonly IEqualityComparer<TKey> keyComparer;
+
+        public DictionaryImplementedWithList()
+        {
+            this.internalKeys = new List<TKey>();
+            this.internalValues = new List<TValue>();
+            this.count = 0;
+            if (typeof(TKey) == typeof(string))
+            {
+                this.keyComparer = new CaseInsensitiveStringComparer();
+            }
+        }
+
+
+        public DictionaryImplementedWithList(IEqualityComparer<TKey> keyComparer):
+            this()
+        {
+            this.keyComparer = keyComparer;
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                if (!this.ContainsKey(key))
+                {
+                    throw new KeyNotFoundException();
+                }
+                return this.internalValues[GetIndexOfKey(key)];
+            }
+
+            set
+            {
+                if (!this.ContainsKey(key))
+                {
+                    throw new KeyNotFoundException();
+                }
+                this.internalValues[GetIndexOfKey(key)] = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.count;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public ICollection<TKey> Keys
+        {
+            get
+            {
+                return this.internalKeys;
+            }
+        }
+
+        public ICollection<TValue> Values
+        {
+            get
+            {
+                return this.internalValues;
+            }
+        }
+
+        public void Add(KeyValuePair<TKey, TValue> item)
+        {
+            Add(item.Key, item.Value);
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (ContainsKey(key))
+            {
+                throw new ArgumentException("Key already exists");
+            }
+            this.internalKeys.Add(key);
+            this.internalValues.Add(value);
+            this.count++;
+        }
+
+        public void Clear()
+        {
+            this.internalKeys.Clear();
+            this.internalValues.Clear();
+            this.count = 0;
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            return
+                ContainsKey(item.Key)
+                && this.internalValues.Contains(item.Value);
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            if (this.keyComparer != null)
+            {
+                return this.internalKeys.Exists(k => this.keyComparer.Equals(key, k));
+            }
+            return this.internalKeys.Contains(key);
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            // TODO :: Validate params and throw if needed
+            int counter = 0;
+            while (counter < this.count)
+            {
+                array[arrayIndex + counter] = new KeyValuePair<TKey, TValue>(this.internalKeys[counter], this.internalValues[counter]);
+                counter++;
+            }
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            int counter = 0;
+            while (counter < this.count)
+            {
+                var kvp = new KeyValuePair<TKey, TValue>(this.internalKeys[counter], this.internalValues[counter]);
+                counter++;
+                yield return kvp;
+            }
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            if (!Contains(item))
+            {
+                return false;
+            }
+            return Remove(item.Key);
+        }
+
+        public bool Remove(TKey key)
+        {
+            if (!ContainsKey(key))
+            {
+                return false;
+            }
+            this.internalValues.RemoveAt(GetIndexOfKey(key));
+            this.internalKeys.Remove(key);
+            this.count--;
+
+            return true;
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            if (!ContainsKey(key))
+            {
+                value = default(TValue);
+                return false;
+            }
+
+            value = this[key];
+            return true;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private int GetIndexOfKey(TKey key)
+        {
+            if (this.keyComparer != null)
+            {
+                return this.internalKeys.FindIndex(k => this.keyComparer.Equals(key, k));
+            }
+            return this.internalKeys.IndexOf(key);
+        }
+    }
+}
